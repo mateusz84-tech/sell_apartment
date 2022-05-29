40e7d8fb2d7e371968698d2fc0c906ad427dcd1b
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2022-05-29 20:44:46 +0200

    Created entity to stored object.

---
+++ sel/src/main/java/matkoc/pl/sel/model/Address.java
@@ -0,0 +1,30 @@
+package matkoc.pl.sel.model;
+
+import lombok.*;
+import javax.persistence.*;
+@Entity
+@Table(name="addresses")
+@Getter @Setter @NoArgsConstructor
+@AllArgsConstructor @ToString @EqualsAndHashCode
+public class Address implements Comparable<Address>{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer address_id;
+    @Column(nullable = false)
+    private String street;
+    @Column(nullable = false)
+    private String streetNumber;
+    @Column(nullable = false)
+    private String city;
+    @Column(nullable = false)
+    private String zipCode;
+    @Override
+    public int compareTo(Address otherAddress) {
+        int result = this.street.compareTo(otherAddress.getStreet());
+        if(result == 0) result = this.streetNumber.compareTo(otherAddress.getStreetNumber());
+        if(result == 0) result = this.city.compareTo(otherAddress.getCity());
+        if(result == 0) result = this.zipCode.compareTo(otherAddress.getZipCode());
+        return result;
+    }
+}
