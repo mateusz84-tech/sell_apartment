8a24e3ef6406967b18c7994f8954d60bffbb9c9d
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2022-05-29 20:17:53 +0200

    Created entity to stored object.

--- sel/src/main/java/matkoc/pl/sel/model/Customer.java
+++ sel/src/main/java/matkoc/pl/sel/model/Customer.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 @Table(name="customers")
 @Getter @Setter @NoArgsConstructor
 @AllArgsConstructor @ToString @EqualsAndHashCode
-public class Customer {
+public class Customer implements Comparable<Customer>{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,5 +17,18 @@ public class Customer {
     private String fName;
     @Column(nullable = false)
     private String lName;
+    @Column(nullable = false, unique = true)
+    private Integer phoneNumber;
+    @Column(nullable = true, unique = true)
+    private Long accountNumber;
+    @Column(nullable = true, unique = true)
+    private String email;
 
+    @Override
+    public int compareTo(Customer otherCustomer) {
+        int result = this.fName.compareTo(otherCustomer.getFName());
+        if(result == 0) result = this.lName.compareTo(otherCustomer.getLName());
+        if(result == 0) result = this.email.compareTo(otherCustomer.getEmail());
+        return result;
+    }
 }
