3d51033fc2647ef73a2b29e1fecf16fc06c2a56a
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2022-06-19 18:40:05 +0200

    Added annotation over field @NotNull and @Access with attribiute PROPERTY to have access to the field by the getter and setter. Resignate with the Lombok annotation by the getter and setter.

--- sel/src/main/java/matkoc/pl/sel/model/Customer.java
+++ sel/src/main/java/matkoc/pl/sel/model/Customer.java
@@ -3,36 +3,96 @@ package matkoc.pl.sel.model;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name="customers")
-@Getter @Setter @NoArgsConstructor
+@NoArgsConstructor
 @AllArgsConstructor @ToString @EqualsAndHashCode
 public class Customer implements Comparable<Customer>{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id_customer;
-    @Column(nullable = false)
+    @NotNull
+    @Access(AccessType.PROPERTY)
+    @Column(name = "FIRST_NAME")
     private String fName;
-    @Column(nullable = false)
+    @NotNull
+    @Access(AccessType.PROPERTY)
+    @Column(name = "LAST_NAME")
     private String lName;
-    @Column(nullable = false, unique = true)
+    @NotNull
+    @Access(AccessType.PROPERTY)
+    @Column(name = "PHONE_NUMBER", unique = true)
     private Integer phoneNumber;
-    @Column(nullable = true, unique = true)
+    @NotNull
+    @Access(AccessType.PROPERTY)
+    @Column(name ="ACCOUNT_NUMBER", unique = true)
     private Long accountNumber;
-    @Column(nullable = true, unique = true)
+    @NotNull
+    @Access(AccessType.PROPERTY)
+    @Column(name = "EMAIL", unique = true)
     private String email;
 
     @OneToOne(optional = false)
     @JoinColumn(name = "address_id")
+    @Access(AccessType.PROPERTY)
     private Address address;
 
     @Override
     public int compareTo(Customer otherCustomer) {
-        int result = this.fName.compareTo(otherCustomer.getFName());
-        if(result == 0) result = this.lName.compareTo(otherCustomer.getLName());
+        int result = this.fName.compareTo(otherCustomer.getfName());
+        if(result == 0) result = this.lName.compareTo(otherCustomer.getlName());
         if(result == 0) result = this.email.compareTo(otherCustomer.getEmail());
         return result;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(Long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
     }
 }
